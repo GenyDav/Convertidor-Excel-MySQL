8b8ac6f0054c77019f9e3a890bd7b4aa72336283
GD <geny.d.mtz@hotmail.com>
2022-01-05 21:19:08 -0600

    Métodos para obtener los nombres de las bases de datos y sus tablas

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -10,6 +10,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -18,18 +19,31 @@ import java.sql.SQLException;
 public class Conexion {
     private Connection conn;
     private DatabaseMetaData info;
+    private ResultSet rs;
     
     public Conexion(String servidor, String usr, String passwd) throws ClassNotFoundException, SQLException{
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://"+servidor+"/",usr,passwd);
-            info = conn.getMetaData();
-            ResultSet rs = info.getCatalogs();
-            System.out.println("List of the catalogs in the database");
-            while(rs.next()) {
-               System.out.println(rs.getString(1));
-            }
+        Class.forName("com.mysql.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://"+servidor+"/",usr,passwd);
+        info = conn.getMetaData();        
     }
-    /*public static void main(String []args){
-        Conexion c = new Conexion();
-    }*/
+    
+    public ArrayList<String> obtenerBasesDeDatos() throws SQLException{
+        ArrayList<String> b = new ArrayList<>();
+        rs = info.getCatalogs();
+        while(rs.next()) {
+           //System.out.println(bases.getString(1));
+           b.add(rs.getString(1));   // añadir la primer columna del ResultSet de Bases de Datos
+        }
+        return b;
+    }
+     
+    public ArrayList<String> obtenerTablas(String nomBase) throws SQLException{
+        ArrayList<String> t = new ArrayList<>();
+        rs = info.getTables(nomBase,null,null,null);
+        while(rs.next()) {
+           System.out.println(rs.getString(3));
+           t.add(rs.getString(3));
+        }
+        return t;
+    }
 }
