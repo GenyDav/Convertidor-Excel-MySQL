a404db147018ce95f02968c00d696e3acdc27bb5
GD <geny.d.mtz@hotmail.com>
2022-01-07 23:30:41 -0600

    Cambio en la clase para liberar la interfaz mientras se cargan los registros

--- src/interfaz/FormatoTabla.java
+++ src/interfaz/FormatoTabla.java
@@ -9,32 +9,45 @@ import conexion.Conexion;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.List;
+import javax.swing.JProgressBar;
 import javax.swing.JTable;
+import javax.swing.SwingWorker;
 import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author Geny
  */
-public class FormatoTabla {
+public class FormatoTabla extends SwingWorker<String,Integer>{
     private DefaultTableModel modelo;
     private JTable tabla;
+    private Conexion conn;
     private ResultSet resultados;
     private ResultSetMetaData metaDatos;
     private int numColumnas;
     private int numRegistros;
+    private String nomBase, nomTabla;
+    private JProgressBar barra;
+    private float progreso;
     
-    public FormatoTabla(JTable t, Conexion c, String nomBase, String nomTabla) throws SQLException{
+    public FormatoTabla(JTable t,Conexion c,String nomBase,String nomTabla,JProgressBar b) throws SQLException{
         modelo = new DefaultTableModel();
         tabla = t;
         tabla.setModel(modelo);
-        resultados = c.obtenerRegistros(nomBase, nomTabla);
-        metaDatos = resultados.getMetaData();
+        //resultados = c.obtenerRegistros(nomBase, nomTabla);
+        conn = c;
+        metaDatos = null;
         numColumnas = 0;
         numRegistros = 0;
-    }
+        this.nomBase = nomBase;
+        this.nomTabla = nomTabla;
+        barra = b;
+        barra.setValue(0);
+        progreso = 0.0F;
+    } 
     
-    public void asignarNombres() throws SQLException{
+    public void asignarNombresColumnas() throws SQLException{
         numColumnas = metaDatos.getColumnCount();   // obtener el número de columnas
         String []columnas = new String[numColumnas];
         for(int i=0;i<numColumnas;i++){             // obtener el nombre de cada columna
@@ -44,9 +57,14 @@ public class FormatoTabla {
     }
     
     public void mostrarInformacion() throws SQLException{
+        float incremento;
+        int numReg = 0;
+        
         resultados.last();
         numRegistros = resultados.getRow();
         System.out.println("numero de registros: "+numRegistros);
+        incremento = 100F/numRegistros;
+        System.out.println("incremento: "+incremento);
         resultados.beforeFirst();
         Object []renglon = new Object[numColumnas];
         while(resultados.next()){           
@@ -54,7 +72,38 @@ public class FormatoTabla {
                 renglon[i] = resultados.getObject(i+1);
             }
             modelo.addRow(renglon);
+            
+            numReg++;
+            progreso = numReg*incremento;
+            //progreso+=incremento;
+            System.out.println("progreso: "+progreso);
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){
+                
+            }
+            publish(Math.round(progreso));
         }      
     }
     
+    @Override
+    public String doInBackground() throws SQLException{
+        resultados = conn.obtenerRegistros(nomBase, nomTabla);
+        metaDatos = resultados.getMetaData();
+        asignarNombresColumnas();
+        mostrarInformacion();
+        return numRegistros + " registros cargados";
+    }
+    
+    @Override
+    protected void done(){
+        
+    }
+    
+    @Override
+    protected void process(List<Integer> chunks){
+        barra.setValue(chunks.get(0));
+        System.out.println("chunk: "+chunks.get(0));
+        barra.setString(Integer.toString(chunks.get(0))+"%");
+    }
 }
