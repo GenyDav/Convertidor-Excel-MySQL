66adcdfc6c86fa3ea68834053d20c9b6a9cba811
GD <geny.d.mtz@hotmail.com>
2022-01-10 18:21:16 -0600

    Método para obtener el número de registros en la consulta

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -10,6 +10,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -20,19 +21,20 @@ public class Conexion {
     private Connection conn;
     private DatabaseMetaData info;
     private ResultSet rs;
+    private Statement s;
     
     public Conexion(String servidor, String usr, String passwd) throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.jdbc.Driver");
         conn = DriverManager.getConnection("jdbc:mysql://"+servidor+"/",usr,passwd);
-        info = conn.getMetaData();        
+        info = conn.getMetaData();  
+        s = conn.createStatement();
     }
     
     public ArrayList<String> obtenerBasesDeDatos() throws SQLException{
         ArrayList<String> b = new ArrayList<>();
         rs = info.getCatalogs();
         while(rs.next()) {
-           //System.out.println(bases.getString(1));
-           b.add(rs.getString(1));   // añadir la primer columna del ResultSet de Bases de Datos
+           b.add(rs.getString(1));   // añadir la columna que tiene el nombre de la BD
         }
         return b;
     }
@@ -41,9 +43,19 @@ public class Conexion {
         ArrayList<String> t = new ArrayList<>();
         rs = info.getTables(nomBase,null,null,null);
         while(rs.next()) {
-           System.out.println(rs.getString(3));
-           t.add(rs.getString(3));
+           t.add(rs.getString(3));  // la tercer columna contiene el nombre de la tabla
         }
         return t;
+    }
+    
+    public ResultSet obtenerRegistros(String base,String tabla) throws SQLException{
+        rs = s.executeQuery("Select * from " + base + "." + tabla + ";");
+        return rs;
+    }
+    
+    public int obtenerNumRegistros(String base,String tabla) throws SQLException{
+        rs = s.executeQuery("Select count(*) from " + base + "." + tabla + ";");
+        rs.first();
+        return Integer.valueOf(rs.getString(1));
     }
 }
