ebde9cd39ec1383d670e49c56e72ee88e81bcae4
GD <geny.d.mtz@hotmail.com>
2022-01-28 12:45:52 -0600

    La clase hereda de SwingWorker para dejar libre la UI

--- src/excel/GeneradorExcel.java
+++ src/excel/GeneradorExcel.java
@@ -14,6 +14,8 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.SwingWorker;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
@@ -31,39 +33,53 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  *
  * @author Geny
  */
-public class GeneradorExcel {
+public class GeneradorExcel extends SwingWorker<Void,Integer>{
     private String nombreBase;
     private ArrayList<String> tablas;
-    private String rutaArch;
     private Conexion conn;
     private Workbook libro;
     private ResultSet resultados;
     private ResultSetMetaData metaDatos;
+    private String rutaArch;
+    private String tipoArch;
+    private JLabel labelProgreso;
     
-    public GeneradorExcel(Conexion c, String base, ArrayList<String>tablas){
+    public GeneradorExcel(Conexion c, String base, ArrayList<String>tablas, JLabel label){
         conn = c;
         nombreBase = base;
         this.tablas = tablas;
         rutaArch = null;
+        tipoArch = null;
+        labelProgreso = label;
     }
     
-    public void crearLibro(String rutaArch){
-        libro = new XSSFWorkbook();
+    public void defInfoArchivo(String rutaArch, String extension){
+        this.rutaArch = rutaArch;
+        tipoArch = extension;
+    }
+    
+    public void crearLibro(){
+        if(tipoArch.equals("xls")){
+            libro = new HSSFWorkbook();
+        }else{
+            libro = new XSSFWorkbook();
+        }
         for(String tablaActual:tablas){
             crearHoja(tablaActual);
         }
-        try  (OutputStream flujoSalida = new FileOutputStream(rutaArch)) {
+        try(OutputStream flujoSalida = new FileOutputStream(rutaArch)){
             libro.write(flujoSalida);
-        } catch (FileNotFoundException ex) {
+        }catch (FileNotFoundException ex){
             // El archivo esta abierto cuando se intentar sobreescribir
             ex.printStackTrace();
-        } catch (IOException ex) {
+        }catch (IOException ex) {
             ex.printStackTrace();
         }
     }
     
     private void crearHoja(String t){
         //System.out.println(t);
+        labelProgreso.setText("Creando hoja "+t);
         Sheet hoja = libro.createSheet(t);
         try {
             resultados = conn.obtenerRegistros(nombreBase,t);
@@ -78,12 +94,47 @@ public class GeneradorExcel {
     private int escribirEncabezados(Sheet hoja) throws SQLException{
         Row renglon = hoja.createRow(0);
         int numColumnas = metaDatos.getColumnCount();
+
+        CellStyle estiloCelda = defEstiloEnc();
         
+        for(int i=0;i<numColumnas;i++){             // obtener el nombre de cada columna
+            Cell celda = renglon.createCell(i);
+            celda.setCellValue(metaDatos.getColumnName(i+1));
+            celda.setCellStyle(estiloCelda);
+        }
+        return numColumnas;
+    }
+    
+    private void escribirDatos(Sheet hoja,int numColumnas) throws SQLException{
+        int renglon = 1;
+        while(resultados.next()){
+            Row reg = hoja.createRow(renglon);
+            for(int i=0;i<numColumnas;i++){
+                Cell celda = reg.createCell(i);
+                Object res = resultados.getObject(i+1);
+                if(res!=null){
+                    celda.setCellValue(res.toString());
+                }else{
+                    //System.out.println("nulo");
+                }
+                /*try{
+                    Thread.sleep(500);
+                }catch(Exception e){}*/
+            }
+            renglon++;
+        }
+        for(int j=0;j<numColumnas;j++){
+            hoja.autoSizeColumn(j);
+        }
+    }
+    
+    private CellStyle defEstiloEnc(){
         Font fuente = libro.createFont();
         fuente.setFontHeightInPoints((short)11);
         fuente.setFontName("Arial Unicode MS");
         fuente.setBold(true);
         fuente.setColor(IndexedColors.WHITE.getIndex());
+        
         CellStyle estiloCelda = libro.createCellStyle();
         estiloCelda.setFont(fuente);
         estiloCelda.setAlignment(HorizontalAlignment.CENTER);
@@ -93,30 +144,12 @@ public class GeneradorExcel {
         estiloCelda.setBorderBottom(BorderStyle.DOUBLE);
         estiloCelda.setRightBorderColor(IndexedColors.WHITE.getIndex());
         estiloCelda.setBorderRight(BorderStyle.THIN);
-        
-        for(int i=0;i<numColumnas;i++){             // obtener el nombre de cada columna
-            Cell celda = renglon.createCell(i);
-            celda.setCellValue(metaDatos.getColumnName(i+1));
-            celda.setCellStyle(estiloCelda);
-        }
-        return numColumnas;
+        return estiloCelda;
     }
-    private void cambiarEstiloEncabezado(){
-        
-    }
-    
-    private void escribirDatos(Sheet hoja,int numColumnas) throws SQLException{
-        int renglon = 1;
-        while(resultados.next()){
-            Row reg = hoja.createRow(renglon);
-            for(int i=0;i<numColumnas;i++){
-                Cell celda = reg.createCell(i);
-                celda.setCellValue(resultados.getObject(i+1).toString());
-            }
-            renglon++;
-        }
-        for(int j=0;j<numColumnas;j++){
-            hoja.autoSizeColumn(j);
-        }
+
+    @Override
+    protected Void doInBackground() throws Exception {
+        crearLibro();
+        return null;
     }
 }
