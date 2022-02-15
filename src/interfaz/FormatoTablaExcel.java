f478ed3a8b1b13c69bfe54df912c002aa6f434dd
GD <geny.d.mtz@hotmail.com>
2022-02-14 19:13:01 -0600

    Validar que la hoja de excel tenga registros

--- src/interfaz/FormatoTablaExcel.java
+++ src/interfaz/FormatoTablaExcel.java
@@ -23,8 +23,8 @@ public class FormatoTablaExcel {
     private JTable tabla;
     private LectorExcel lector;
     private XSSFSheet hoja;
-    private int indiceRengInicio, indiceRengFinal;
-    private int indiceColInicio, indiceColFinal;
+    private int indiceRengFinal;
+    private int indiceColInicio;
     
     private int indice; // indice de la hoja que se muestra en pantalla
     private int numColumnas; // de la hoja actual
@@ -37,10 +37,8 @@ public class FormatoTablaExcel {
         this.indice = indiceHoja;
         hoja = lector.getLibro().getSheetAt(indice);
         numColumnas = 0;
-        indiceRengInicio = 0;
         indiceRengFinal = 0;
         indiceColInicio = 0;
-        indiceColFinal = 0;
     }
     
     public final void asignarNombresColumnas(){
@@ -50,42 +48,39 @@ public class FormatoTablaExcel {
         if(hoja.getPhysicalNumberOfRows()==1){ // si solo hay un renglón en la hoja
             encabezado = hoja.getRow(hoja.getLastRowNum());
         }else{
-            System.out.println("encabezado: "+hoja.getFirstRowNum());
+            //System.out.println("encabezado: "+hoja.getFirstRowNum());
             encabezado = hoja.getRow(hoja.getFirstRowNum());
-            indiceRengInicio = hoja.getFirstRowNum();
         }
         indiceColInicio = encabezado.getFirstCellNum();
-        System.out.println("Primer columna: "+encabezado.getFirstCellNum());
-        indiceColFinal = encabezado.getLastCellNum();
-        System.out.println("Ultima columna: "+encabezado.getLastCellNum());
+        //System.out.println("Primer columna: "+encabezado.getFirstCellNum());
        
         Iterator<Cell> iterador = encabezado.cellIterator();
         while(iterador.hasNext()){
             encabezadosTabla.add(iterador.next());
         }
         numColumnas = encabezado.getPhysicalNumberOfCells();
-        System.out.println("Columnas: "+encabezado.getPhysicalNumberOfCells());
+        //System.out.println("Columnas: "+encabezado.getPhysicalNumberOfCells());
         modelo.setColumnIdentifiers(encabezadosTabla.toArray());
     }
     
     public final void escribirCeldas(){
-        indiceRengFinal = hoja.getLastRowNum();
-        System.out.println("Ultima linea: "+indiceRengFinal);
-        
-        
-        Object []renglon = new Object[numColumnas];
-        Row renglonArch;
-
-        Iterator<Row> iteradorRenglon = hoja.rowIterator();
-        renglonArch = iteradorRenglon.next();
-        while(iteradorRenglon.hasNext()){
-            renglonArch = iteradorRenglon.next();
-            for(int i=0;i<numColumnas;i++){
-                System.out.println("Columna: "+(i+indiceColInicio));
-                renglon[i] = renglonArch.getCell(i+indiceColInicio);
+        if(hoja.getPhysicalNumberOfRows()>1){
+            indiceRengFinal = hoja.getLastRowNum();
+            //System.out.println("Ultima linea: "+indiceRengFinal);
+            
+            Object []renglon = new Object[numColumnas];
+            Row renglonArch;
+            Iterator<Row> iteradorRenglon = hoja.rowIterator();
+            renglonArch = iteradorRenglon.next(); // saltar el encabezado
+            while(iteradorRenglon.hasNext()){
+                renglonArch = iteradorRenglon.next();
+                for(int i=0;i<numColumnas;i++){
+                    //System.out.println("Columna: "+(i+indiceColInicio));
+                    renglon[i] = renglonArch.getCell(i+indiceColInicio);
+                }
+                modelo.addRow(renglon);
             }
-            modelo.addRow(renglon);
-        }
+        }   
     }
     
     public final void llenarTabla(){
