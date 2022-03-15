848590ef28a1852ec8d705f9ad6f9845a237ed3f
GD <geny.d.mtz@hotmail.com>
2022-03-14 22:44:30 -0600

    Creación de la clase que contiene todos los nombres de los tipos de datos utilizados para crear la base de datos

---
+++ src/interfaz/Tipo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz;
+
+/**
+ *
+ * @author Geny
+ */
+public class Tipo {
+    public static String[] TIPO = {
+        "TINYINT","SMALLINT","MEDIUMINT","INT","BIGINT",
+        "FLOAT","DOUBLE","DECIMAL",
+        "CHAR","VARCHAR",
+        "BINARY","VARBINARY",
+        "TINYBLOB","MEDIUMBLOB","BLOB","LONGBLOB",
+        "TINYTEXT","TEXT","MEDIUMTEXT","LONGTEXT",
+        "SET","ENUM",
+        "DATE","DATETIME","TIME","TIMESTAMP","YEAR"
+    }; 
+}
