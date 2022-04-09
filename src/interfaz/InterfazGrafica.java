e3b29c51a4625dff16ffb0f258330cb44ba83d93
GD <geny.d.mtz@hotmail.com>
2022-04-08 21:16:43 -0500

    Eliminación de método obsoleto

--- src/interfaz/InterfazGrafica.java
+++ src/interfaz/InterfazGrafica.java
@@ -14,11 +14,8 @@ import java.awt.event.ItemEvent;
 import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -28,13 +25,11 @@ import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
-import javax.swing.JProgressBar;
 import javax.swing.JTable;
 import javax.swing.UIManager;
 import javax.swing.filechooser.FileSystemView;
 import javax.swing.table.DefaultTableModel;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
+
 
 /**
  *
@@ -1371,54 +1366,6 @@ public class InterfazGrafica extends javax.swing.JFrame {
             e.printStackTrace();
         }
     }  
-      
-    /*public String crearScriptRegistros(String nomBase,TablaLista hoja){
-        Sheet hojaActual = lector.getLibro().getSheetAt(hoja.getPosicion());
-        Row encabezado = hojaActual.getRow(hojaActual.getFirstRowNum());
-        int numColumnas = encabezado.getPhysicalNumberOfCells();
-        int numRenglones = hojaActual.getPhysicalNumberOfRows();
-        int indiceColInicio = encabezado.getFirstCellNum();
-        
-        //int []TiposDatosHoja = new int[numColumnas];
-        //for(int i=0;i<numColumnas;i++){
-        //    TiposDatosHoja[i] = hoja.obtenerColumnas().get(i).getTipo();
-        //    System.out.println(Tipo.TIPO[TiposDatosHoja[i]]);
-        //}
-        
-        System.out.println("Número de registros: "+hojaActual.getPhysicalNumberOfRows()); // contando el encabezado
-        String scriptInsertar = "";
-        
-        if(numRenglones>1){
-            Row renglonArch;
-            Object []renglon = new Object[numColumnas];
-            Iterator<Row> iteradorRenglon = hojaActual.rowIterator();
-            
-            scriptInsertar = "INSERT INTO "+nomBase+"."+hoja.getNombre()+" VALUES \n";
-            
-            renglonArch = iteradorRenglon.next(); // saltar el encabezado
-            while(iteradorRenglon.hasNext()){
-                renglonArch = iteradorRenglon.next();
-                scriptInsertar += "(";
-                for(int i=0;i<numColumnas;i++){
-                    //System.out.println("Columna: "+(i+indiceColInicio));
-                    renglon[i] = renglonArch.getCell(i+indiceColInicio);
-                    scriptInsertar += "'"+renglon[i]+"'";
-                    if(i!=numColumnas-1){
-                        scriptInsertar += ",";
-                    }
-                    //System.out.print("["+renglon[i]+"]");
-                }  
-                scriptInsertar += ")";
-                if(iteradorRenglon.hasNext()){
-                    scriptInsertar += ",\n";
-                }else{
-                    scriptInsertar += ";";
-                }   
-            }
-            System.out.println(scriptInsertar);
-        }  
-        return scriptInsertar;
-    }*/
     
     public <T extends ElementoLista> void borrarElemento(JList seleccion,DefaultListModel modeloLista,ArrayList<T> lista,JLabel label,JButton btnQuitar,JButton btnBorrar,JButton btnOperacion,String msj,String msjPlural){
         int []elemBorrados = seleccion.getSelectedIndices(); // posiciones de los elementos a eliminar
