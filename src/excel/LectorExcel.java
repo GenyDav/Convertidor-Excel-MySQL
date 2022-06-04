8c020266c9a20a0142440305aed26eeaedee87b1
GD <geny.d.mtz@hotmail.com>
2022-06-03 21:01:51 -0500

    Se añadió un nuevo constructor a la clase

--- src/excel/LectorExcel.java
+++ src/excel/LectorExcel.java
@@ -5,8 +5,9 @@
  */
 package excel;
 
-import interfaz.FormatoTablaExcel;
-import interfaz.InfoColumna;
+import datos.InfoColumna;
+import datos.Tipo;
+import interfaz.PanelImport;
 import interfaz.TablaLista;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -20,7 +21,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
-import javax.swing.JTable;
 import javax.swing.SwingWorker;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -33,83 +33,83 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
  * @author Geny
  */
 public class LectorExcel extends SwingWorker<Void,Void>{  
-    private String ruta;
-    //private FileInputStream flujoEntrada;
-    private Workbook libro;
-    private JComboBox comboHojas;
-    private JTable tabla;
-    private JLabel etiqueta;
-    private JLabel nombreArchivo;
-    private String nombre;
-    private JButton botonAbrir;
-    private JButton btnTipos;
+    private String ruta;            // ruta del archivo que va a cargar
+    private String nombre;          // nombre del archivo
+    private Workbook libro;         // archivo Excel
+    private JComboBox comboHojas;   // comboBox en donde se añaden los nombres de las hojas del archivo
+    private JLabel etiquetaReg;     // etiqueta donde se muestra el número de renglones de la hoja 
+    private JLabel labelArchivo;    // etiqueta para el nombre del archivo
+    private JButton btnAbrir;
+    private JButton btnTipos;       
     private JButton btnImportar;
-    private JRadioButton opc1,opc2;
+    private JRadioButton opcTablasComp,opcTablasSel;
     private JFrame ventana;
-    private ArrayList<TablaLista> hojas;
+    private ArrayList<TablaLista> hojas; // lista de hojas encontradas en el archivo
+   
+    public LectorExcel(JFrame ventana, PanelImport panelImport){
+        ruta = panelImport.getRutaArch();//
+        comboHojas = panelImport.getComboHojas();
+        this.ventana = ventana;
+        etiquetaReg = panelImport.getLabelRegExcel();
+        labelArchivo = panelImport.getLabelArchivo();
+        nombre = panelImport.getNomArch();//
+        btnAbrir = panelImport.getBtnAbrir();
+        btnImportar = panelImport.getBtnImportar();
+        this.opcTablasComp = panelImport.getOpcHojasCompletas();
+        this.opcTablasSel = panelImport.getOpcHojasSel();
+        btnTipos = panelImport.getBtnTipos();
+        hojas = panelImport.getListaHojas();
+    }
     
-    public LectorExcel(JFrame ventana,JComboBox combo,JTable tabla,JLabel label,JLabel nomArch,JButton btn,String ruta,String nombre,JButton btnTipos,ArrayList<TablaLista>hojas,JButton btnImport,JRadioButton opc1,JRadioButton opc2){
-        this.ruta = ruta;
+    public LectorExcel(JFrame ventana,JComboBox combo,JLabel label,JLabel nomArch,JButton btn,String ruta,String nombre,JButton btnTipos,ArrayList<TablaLista>hojas,JButton btnImport,JRadioButton opc1,JRadioButton opc2){
+        this.ruta = ruta;//
         comboHojas = combo;
         this.ventana = ventana;
-        this.tabla = tabla;
-        etiqueta = label;
-        nombreArchivo = nomArch;
-        this.nombre = nombre;
-        botonAbrir = btn;
+        etiquetaReg = label;
+        labelArchivo = nomArch;
+        this.nombre = nombre;//
+        btnAbrir = btn;
         btnImportar = btnImport;
-        this.opc1 = opc1;
-        this.opc2 = opc2;
+        this.opcTablasComp = opc1;
+        this.opcTablasSel = opc2;
         this.btnTipos = btnTipos;
         this.hojas = hojas;
     }
-
-    public void setRuta(String ruta){
-        this.ruta = ruta;
-    }
-    
-    public JTable getTabla(){
-        return tabla;
-    }
     
     public JLabel getLabel(){
-        return etiqueta;
+        return etiquetaReg;
     }
     
     public void leerArchivo() throws IOException{
+        Sheet hojaActual;
+        Row encabezado;
+        int numeroColumna;
         try{
-            //flujoEntrada = new FileInputStream(new File(ruta));
             libro = WorkbookFactory.create(new File(ruta));
-            //libro = new XSSFWorkbook(flujoEntrada);
-            //System.out.println("Archivo creado...");
-            
-            Sheet hojaActual;
-            Row encabezado; 
-
             for(int i=0;i<libro.getNumberOfSheets();i++){
-                //System.out.println("Llenando hoja...");
-                int numCol = 0;
                 hojaActual = libro.getSheetAt(i);
+                // Crear un nuevo elemento 'hoja' y añadirlo a la estructura de datos
                 hojas.add(new TablaLista(hojaActual.getSheetName(),i));
-                //System.out.println("encabezado: "+hoja.getFirstRowNum());
-                encabezado = hojaActual.getRow(hojaActual.getFirstRowNum());
-
-                Iterator<Cell> iterador = encabezado.cellIterator();
-                while(iterador.hasNext()){
-                    //System.out.println(iterador.next().toString());
-                    //hojas.get(i).agregarColumna(new InfoColumna(iterador.next().toString()));
-                    // identificar la primer columna y preasignarle el tipo de dato int
-                    if(numCol==0){
-                       hojas.get(i).agregarColumna(new InfoColumna(iterador.next().toString(),3));
-                    }else{
-                        hojas.get(i).agregarColumna(new InfoColumna(iterador.next().toString()));
+                // Verificar si la hoja no está vacía
+                if(hojaActual.getPhysicalNumberOfRows()>0){ 
+                    numeroColumna = 0;
+                    // Leer el primer renglón de la hoja
+                    encabezado = hojaActual.getRow(hojaActual.getFirstRowNum());
+                    Iterator<Cell> iterador = encabezado.cellIterator();
+                    while(iterador.hasNext()){
+                        // Identificar la primer columna y preasignarle el tipo de dato int
+                        // a las demás columnas se les asigna el tipo varchar
+                        if(numeroColumna==0){
+                           hojas.get(i).agregarColumna(new InfoColumna(iterador.next().toString(),Tipo.INT));
+                        }else{
+                            hojas.get(i).agregarColumna(new InfoColumna(iterador.next().toString(),Tipo.VARCHAR));
+                        }
+                        numeroColumna++;
                     }
-                    numCol++;
                 }
-                numCol = 0;
             }      
-            new FormatoTablaExcel(0,this).execute();
         }catch(FileNotFoundException e){
+            etiquetaReg.setIcon(null);
             JOptionPane.showMessageDialog(
                 ventana, 
                 "El archivo está siendo utilizado por otro programa.  "
@@ -123,21 +123,18 @@ public class LectorExcel extends SwingWorker<Void,Void>{
         }
     }
     
-    public int obtenerNumHojas(){
+    /*public int obtenerNumHojas(){
         //System.out.println("Obteniendo número de hojas..."+libro.getNumberOfSheets());
         return libro.getNumberOfSheets();
-    }
+    }*/
     
+    /**
+     * Lee los nombres de las hojas del archivo y los añade al comboBox
+     * correspondiente.
+     */
     public void obtenerNombresHojas(){
-        //System.out.println("Obteniendo nombres de hojas...");
-        try{
-            for(int i=0;i<libro.getNumberOfSheets();i++){
-                //System.out.println("i: "+i+ " "+libro.getSheetName(i));
-                comboHojas.addItem(libro.getSheetName(i));
-            }
-        }catch(Exception e){
-            System.out.println("Error:");
-            e.printStackTrace();
+        for(int i=0;i<libro.getNumberOfSheets();i++){
+            comboHojas.addItem(libro.getSheetName(i)); 
         }
     }
     
@@ -157,33 +154,35 @@ public class LectorExcel extends SwingWorker<Void,Void>{
         return hojas;
     }
     
+    /**
+     * Habilita todos los elementos de la interfaz despues de que finaliza el
+     * proceso de apertura del archivo.
+     */
+    private void habilitarControles(){
+        comboHojas.setEnabled(true);
+        btnAbrir.setEnabled(true);
+        btnTipos.setEnabled(true);
+        opcTablasComp.setEnabled(true);
+        opcTablasSel.setEnabled(true);
+        btnImportar.setEnabled(true);
+    }
+    
     @Override
     public Void doInBackground() throws IOException{
-        //System.out.println("Iniciando lectura...");
-        etiqueta.setText("Leyendo archivo...");
-        etiqueta.setIcon(new ImageIcon(getClass().getResource("/imagenes/loading2.gif")));
+        etiquetaReg.setText("Leyendo archivo...");
+        etiquetaReg.setIcon(new ImageIcon(getClass().getResource("/imagenes/loading2.gif")));
         leerArchivo();
-        nombreArchivo.setText("  "+nombre);
+        labelArchivo.setText("  " + nombre);
         obtenerNombresHojas();
-        comboHojas.setEnabled(true);
-        botonAbrir.setEnabled(true);
-        btnTipos.setEnabled(true);
-        opc1.setEnabled(true);
-        //opc1.setSelected(true);
-        opc2.setEnabled(true);
-        btnImportar.setEnabled(true);
-        /*if(seleccion.isSelected()){
-            btnImportar.setEnabled(true);
-        }
+        habilitarControles();
         
-        /*TablaLista tabla;
+        TablaLista tabla;
         System.out.println("Información");
         for(int i=0;i<hojas.size();i++){
             tabla = hojas.get(i);
             tabla.mostrarColumnas();
             System.out.println();
-        }  */
-        //etiqueta.setIcon(null);
+        }  
         return null;
     }
 }
