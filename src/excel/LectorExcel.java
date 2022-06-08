d3dc5ff5a60fee2b82a423d7c66e7ebaf0ed3ab1
GD <geny.d.mtz@hotmail.com>
2022-06-08 16:28:27 -0500

    Documentación de la clase añadida

--- src/excel/LectorExcel.java
+++ src/excel/LectorExcel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package excel;
 
 import datos.InfoColumna;
@@ -29,30 +24,39 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 /**
- *
+ * Clase encargada de leer el contenido del archivo Excel y crear una lista que
+ * contiene la información de cada hoja necesaria para crear una tabla MySQL.
  * @author Geny
+ * @version 1.0
  */
 public class LectorExcel extends SwingWorker<Void,Void>{  
-    private String ruta;            // ruta del archivo que va a cargar
-    private String nombre;          // nombre del archivo
-    private Workbook libro;         // archivo Excel
+    private String ruta;                    // ruta del archivo que va a cargar
+    private String nombre;                  // nombre del archivo
+    private Workbook libro;                 // archivo Excel
+    private ArrayList<TablaLista> hojas;    // lista de hojas encontradas en el archivo
+    
     private JComboBox comboHojas;   // comboBox en donde se añaden los nombres de las hojas del archivo
     private JLabel etiquetaReg;     // etiqueta donde se muestra el número de renglones de la hoja 
     private JLabel labelArchivo;    // etiqueta para el nombre del archivo
     private JButton btnAbrir;
-    private JButton btnTipos;       
+    private JButton btnTipos;       // botón para configurar los tipos de datos de las hojas
     private JButton btnImportar;
     private JRadioButton opcTablasComp,opcTablasSel;
-    private JFrame ventana;
-    private ArrayList<TablaLista> hojas; // lista de hojas encontradas en el archivo
+    private JFrame ventana;         // ventana sobre la que se muestra el programa
    
+    /**
+     * Crea el objeto capaz de obtener la información de un archivo de Excel.
+     * @param ventana Frame sobre el que se ejecuta el programa
+     * @param panelImport Panel que contiene todos los elementos para importar un
+     * archivo.
+     */
     public LectorExcel(JFrame ventana, PanelImport panelImport){
-        ruta = panelImport.getRutaArch();//
+        ruta = panelImport.getRutaArch();
         comboHojas = panelImport.getComboHojas();
         this.ventana = ventana;
         etiquetaReg = panelImport.getLabelRegExcel();
         labelArchivo = panelImport.getLabelArchivo();
-        nombre = panelImport.getNomArch();//
+        nombre = panelImport.getNomArch();
         btnAbrir = panelImport.getBtnAbrir();
         btnImportar = panelImport.getBtnImportar();
         this.opcTablasComp = panelImport.getOpcHojasCompletas();
@@ -61,50 +65,56 @@ public class LectorExcel extends SwingWorker<Void,Void>{
         hojas = panelImport.getListaHojas();
     }
     
-    public LectorExcel(JFrame ventana,JComboBox combo,JLabel label,JLabel nomArch,JButton btn,String ruta,String nombre,JButton btnTipos,ArrayList<TablaLista>hojas,JButton btnImport,JRadioButton opc1,JRadioButton opc2){
-        this.ruta = ruta;//
-        comboHojas = combo;
-        this.ventana = ventana;
-        etiquetaReg = label;
-        labelArchivo = nomArch;
-        this.nombre = nombre;//
-        btnAbrir = btn;
-        btnImportar = btnImport;
-        this.opcTablasComp = opc1;
-        this.opcTablasSel = opc2;
-        this.btnTipos = btnTipos;
-        this.hojas = hojas;
+    /**
+     * Método que devuelve el objeto que representa al archivo de Excel.
+     * @return objeto de tipo Workbook que contiene la información del archivo abierto.
+     */
+    public Workbook getLibro(){
+        return libro;
     }
     
-    public JLabel getLabel(){
-        return etiquetaReg;
+    /**
+     * Devuelve la hoja del archivo cuyo índice se indica en el parámetro.
+     * @param indice Índice de la hoja que se quiere obtener
+     * @return Hoja del archivo Excel.
+     */
+    public Sheet getHoja(int indice){
+        return libro.getSheetAt(indice);
     }
     
+    /**
+     * Abre el archivo seleccionado por el usuario y crea la estructura de datos
+     * que representa a las hojas del archivo y la lista de columnas que pertenecen
+     * a cada hoja.
+     * @throws IOException Error lanzado si no se tiene acceso al archivo. 
+     */
     public void leerArchivo() throws IOException{
         Sheet hojaActual;
         Row encabezado;
-        int numeroColumna;
+        int indiceColumna;
         try{
+            // Abrir el archivo
             libro = WorkbookFactory.create(new File(ruta));
+            // Recorrer cada hoja para crear la estructura de datos
             for(int i=0;i<libro.getNumberOfSheets();i++){
                 hojaActual = libro.getSheetAt(i);
-                // Crear un nuevo elemento 'hoja' y añadirlo a la estructura de datos
+                // Crear un nuevo elemento 'hoja' y añadirlo a la estructura
                 hojas.add(new TablaLista(hojaActual.getSheetName(),i));
-                // Verificar si la hoja no está vacía
+                // Verificar si la hoja no está vacía para agregar las columnas a la estructura
                 if(hojaActual.getPhysicalNumberOfRows()>0){ 
-                    numeroColumna = 0;
-                    // Leer el primer renglón de la hoja
+                    indiceColumna = 0;
+                    // Leer el primer renglón de la hoja (que será el encabezado de la tabla)
                     encabezado = hojaActual.getRow(hojaActual.getFirstRowNum());
                     Iterator<Cell> iterador = encabezado.cellIterator();
                     while(iterador.hasNext()){
                         // Identificar la primer columna y preasignarle el tipo de dato int
                         // a las demás columnas se les asigna el tipo varchar
-                        if(numeroColumna==0){
+                        if(indiceColumna==0){
                            hojas.get(i).agregarColumna(new InfoColumna(iterador.next().toString(),Tipo.INT));
                         }else{
                             hojas.get(i).agregarColumna(new InfoColumna(iterador.next().toString(),Tipo.VARCHAR));
                         }
-                        numeroColumna++;
+                        indiceColumna++;
                     }
                 }
             }      
@@ -117,17 +127,11 @@ public class LectorExcel extends SwingWorker<Void,Void>{
                 "No se puede abrir el archivo", 
                 JOptionPane.INFORMATION_MESSAGE
             );
-            e.printStackTrace();
-        }catch(Exception ex2){
-            ex2.printStackTrace();
+            labelArchivo.setText("");
+            //e.printStackTrace();
         }
     }
-    
-    /*public int obtenerNumHojas(){
-        //System.out.println("Obteniendo número de hojas..."+libro.getNumberOfSheets());
-        return libro.getNumberOfSheets();
-    }*/
-    
+
     /**
      * Lee los nombres de las hojas del archivo y los añade al comboBox
      * correspondiente.
@@ -136,22 +140,6 @@ public class LectorExcel extends SwingWorker<Void,Void>{
         for(int i=0;i<libro.getNumberOfSheets();i++){
             comboHojas.addItem(libro.getSheetName(i)); 
         }
-    }
-    
-    public Workbook getLibro(){
-        return libro;
-    }
-    
-    public void cerrarArchivo(){
-        try {
-            libro.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-    
-    public ArrayList<TablaLista> obtenerInfoArchivo(){
-        return hojas;
     }
     
     /**
@@ -167,6 +155,24 @@ public class LectorExcel extends SwingWorker<Void,Void>{
         btnImportar.setEnabled(true);
     }
     
+    /**
+     * Cierra el flujo de datos del archivo.
+     */
+    public void cerrarArchivo(){
+        try{
+            libro.close();
+        }catch (IOException ex) {
+            //ex.printStackTrace();
+        }
+    }
+    
+    /**
+     * Ejecuta en segundo plano la lectura del archivo, añade los nombres de las
+     * hojas al combo correspondiente y actualiza los demás elementos de la interfaz
+     * para que puedan ser utilizados.
+     * @return null
+     * @throws IOException Error en la entrada/salida de datos
+     */
     @Override
     public Void doInBackground() throws IOException{
         etiquetaReg.setText("Leyendo archivo...");
@@ -174,15 +180,7 @@ public class LectorExcel extends SwingWorker<Void,Void>{
         leerArchivo();
         labelArchivo.setText("  " + nombre);
         obtenerNombresHojas();
-        habilitarControles();
-        
-        TablaLista tabla;
-        System.out.println("Información");
-        for(int i=0;i<hojas.size();i++){
-            tabla = hojas.get(i);
-            tabla.mostrarColumnas();
-            System.out.println();
-        }  
+        habilitarControles();       
         return null;
     }
 }
